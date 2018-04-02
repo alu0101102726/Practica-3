f34c8e02a60a5755506d8ccc71a4c52c66a17fec
carlosdiazc99 <37936358+carlosdiazc99@users.noreply.github.com>
2018-04-02 11:57:54 +0100

    Update sparse_matrix.cpp

--- sparse_matrix.cpp
+++ sparse_matrix.cpp
@@ -1,23 +1,26 @@
+
+
 sparse_matrix_t::sparse_matrix_t(const matrix_t& M, double eps, int conf){
 	m_(M.get_m()),
 	n_(M.get_n()),
 	conf_(conf),
 	dispersa()
 {
+	int contador;
 	if(conf==COL_CONF){
 		dispersa_.resize(n_),
 		for(int j=1; j<n_;j++){
 			contador=0;
 			for(int i=1; i<m_; i++){
-				if(M(i,j!=0)){
+				if(M(i,j)!=0){
 					contador++;
 				}
 			}
-			dispersa_(i,j).resize(contador);
+			dispersa_[j-1].resize(contador);
 			contador=0;
 			for(int i=1; i<m_;i++){
 				if(M(i,j)!=0){
-					dispersa_(j-1)(contador).set(i,M(i,j));
+					dispersa_[j-1][contador].set(i,M(i,j));
 					contador++;
 				}
 			}
