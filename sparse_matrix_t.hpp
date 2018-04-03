e908a3c674af927e4eb68a7de9f8c49ba5bc5aa7
carlosdiazc99 <37936358+carlosdiazc99@users.noreply.github.com>
2018-04-03 18:13:06 +0100

    Update sparse_matrix_t.hpp

--- sparse_matrix_t.hpp
+++ sparse_matrix_t.hpp
@@ -26,7 +26,25 @@ enum {COL_CONF, ROW_CONF};
 		conf_(conf),
 		dispersa_()
 		{
-
+		int contador;
+		if(conf==COL_CONF){
+			dispersa_.resize(n_),
+			for(int j=1; j<n_;j++){
+				contador=0;
+				for(int i=1; i<m_; i++){
+					if(M(i,j)!=0){
+						contador++;
+					}
+				}
+				dispersa_[j-1].resize(contador);
+				contador=0;
+				for(int i=1; i<m_;i++){
+					if(M(i,j)!=0){
+						dispersa_[j-1][contador].set(i,M(i,j));
+						contador++;
+					}
+				}
+			}
 		}
 
 		~sparse_matrix_t(void) {}
